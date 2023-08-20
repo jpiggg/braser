392a0f0d9c4d078abc8ad93081ad29259d638964
Julia Gracheva <jpig@Julias-MacBook-Pro.local>
2023-08-20 22:41:09 +0400

    some fixes

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -7,8 +7,9 @@ pub fn to_object(pairs: &Vec<Node>) -> js_sys::Object {
     let object = js_sys::Object::new();
 
     for pair in pairs.iter() {
-        let key: js_sys::JsString = js_sys::JsString::from(pair.value.as_str()).into();
-        let _ = js_sys::Reflect::set(&object, &key, &compile(pair.children.first().unwrap()));
+        let str: js_sys::JsString = js_sys::JsString::from(pair.value.as_str()).into();
+       let key = str.substr(1, (str.length() - 2).try_into().unwrap()).into();
+        let _ = js_sys::Reflect::set(&object, &key, &run(pair.children.first().unwrap()));
     }
 
     object
@@ -18,68 +19,57 @@ pub fn to_array(items: &Vec<Node>) -> js_sys::Array {
     let result = js_sys::Array::new();
 
     for item in items.iter() {
-        result.push(&compile(item));
+        result.push(&run(item));
     }
 
     result
 }
 
-pub fn compile(node: &Node) -> JsValue{
-        let res: JsValue = match node.kind.as_str() {
-            "object" => {
-                to_object(&node.children).into()
-            },
-            "array" => {
-                to_array(&node.children).into()
-            },
-            "string" => {
-                js_sys::JsString::from(node.value.as_str()).into()
-            },
-            "number" => {
-                js_sys::Number::from_str(node.value.as_str()).unwrap().into()
-            },
-            "bigint" => {
-                js_sys::BigInt::from_str(&node.value.as_str()).unwrap().into()
-            },
-            "infinity" => {
-                if node.value.as_str() == "-" {
-                    js_sys::Number::NEGATIVE_INFINITY.into()
-                } else {
-                    js_sys::Number::POSITIVE_INFINITY.into()
-                }
-            },
-            "null" => {
-                JsValue::null()
-            },
-            "undefined" => {
-                JsValue::undefined()
-            },
-            "nan" => {
-                JsValue::from(f64::NAN)
+pub fn run(node: &Node) -> JsValue {
+    let res: JsValue = match node.kind.as_str() {
+        "object" => {
+            to_object(&node.children).into()
+        },
+        "array" => {
+            to_array(&node.children).into()
+        },
+        "string" => {
+            let str = js_sys::JsString::from(node.value.as_str());
+            str.substr(1, (str.length() - 2).try_into().unwrap()).into()
+        },
+        "number" => {
+            js_sys::Number::from_str(node.value.as_str()).unwrap().into()
+        },
+        "bigint" => {
+            js_sys::BigInt::from_str(&node.value.as_str()).unwrap().into()
+        },
+        "infinity" => {
+            if node.value.as_str() == "-" {
+                js_sys::Number::NEGATIVE_INFINITY.into()
+            } else {
+                js_sys::Number::POSITIVE_INFINITY.into()
             }
-            "date" => {
-                js_sys::Date::new(&js_sys::JsString::from(node.value.as_str())).into()
-            },
-            "boolean" => {
-                JsValue::as_bool(&js_sys::JsString::from(node.value.as_str())).into()
-            },
-            // "function" => {},
-            _ => {
-                panic!("There is no trasformers for specified node kind {:?}", node.kind.as_str());
-            }
-        };
+        },
+        "null" => {
+            JsValue::null()
+        },
+        "undefined" => {
+            JsValue::undefined()
+        },
+        "nan" => {
+            JsValue::from(f64::NAN)
+        }
+        "date" => {
+            js_sys::Date::new(&js_sys::JsString::from(node.value.as_str())).into()
+        },
+        "boolean" => {
+            JsValue::as_bool(&js_sys::JsString::from(node.value.as_str())).into()
+        },
+        // "function" => {},
+        _ => {
+            panic!("There is no trasformers for specified node kind {:?}", node.kind.as_str());
+        }
+    };
 
-    res
-
-}
-
-pub fn run(nodes: &Vec<Node>) -> Vec<JsValue> {
-    let mut res: Vec<JsValue> = vec![];
-
-    for node in nodes.iter() {
-        res.push(compile(node));
-    }
-
-    res
-
+res
 }
