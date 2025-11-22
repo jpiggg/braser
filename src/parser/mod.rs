232f3832da464fab67f9d5ba5fb26d4933aefba0
jpig <isdolinskayaym@gmail.com>
2025-11-22 19:01:08 +0100

    wip

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,36 +2,58 @@ use pest::Parser;
 
 use pest_derive;
 use from_pest;
-use pest_ast;
 use pest;
 
 #[derive(pest_derive::Parser)]
 #[grammar = "grammar/eson.pest"]
 pub struct ESonParser;
 
-mod ast {
+pub mod ast {
     use super::Rule;
     use pest::Span;
 
     fn span_into_str(span: Span) -> &str {
+        println!("Spaaaaan --------> ${:?}", span);
         span.as_str()
     }
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Boolean))]
-    pub struct Boolean {}
+    pub struct Boolean {
+        #[pest_ast(outer(with(span_into_str), with(str::parse), with(Result::unwrap)))]
+        value: bool
+    }
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::String))]
-    pub struct String {}
+    pub struct String {
+        #[pest_ast(outer(with(span_into_str), with(str::to_string)))]
+        value: std::string::String
+    }
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Number))]
-    pub struct Number {}
+    pub struct Number {
+        #[pest_ast(outer(with(span_into_str), with(str::parse), with(Result::unwrap)))]
+        value: f64
+    }
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Infinity))]
     pub struct Infinity {}
+
+    #[derive(Debug, pest_ast::FromPest)]
+    #[pest_ast(rule(Rule::undefined))]
+    pub struct Undefined {}
+
+    #[derive(Debug, pest_ast::FromPest)]
+    #[pest_ast(rule(Rule::NaN))]
+    pub struct NaN {}
+
+
+    #[derive(Debug, pest_ast::FromPest)]
+    #[pest_ast(rule(Rule::null))]
+    pub struct Null {}
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::BigInt))]
@@ -40,31 +62,47 @@ mod ast {
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::value))]
     pub enum Value {
-        Object(Object),
-        Array(Array),
+        Object(Box<Object>),
+        Array(Box<Array>),
         String(String),
         Number(Number),
         Boolean(Boolean),
-        Null,
-        // #[pest_ast(outer(with(span_into_str), with(str::to_string)))]
-        // _undefined(Undefined),
-        NaN,
+        Null(Null),
+        Undefined(Undefined),
+        NaN(NaN),
         Infinity(Infinity),
         BigInt(BigInt)
     }
 
+     #[derive(Debug, pest_ast::FromPest)]
+    #[pest_ast(rule(Rule::COMMENT))]
+    pub struct Comment {
+        #[pest_ast(outer(with(span_into_str), with(str::to_string)))]
+        value: std::string::String
+    }
+
+    #[derive(Debug, pest_ast::FromPest)]
+    #[pest_ast(rule(Rule::key))]
+    pub struct Key {
+        #[pest_ast(outer(with(span_into_str), with(str::to_string)))]
+        value: std::string::String
+    }
+
+    //@TODO: научиться парсить комментарии, если перед ними идет запятая
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::pair))]
     pub struct Pair {
-        #[pest_ast(outer(with(span_into_str), with(str::to_string)))]
-        pub key: std::string::String,
-        pub value: Value
+        pub key: Option<Key>,
+        pub value: Option<Value>,
+        comment: Option<Comment>
     }
 
     #[derive(Debug, pest_ast::FromPest)]
+
     #[pest_ast(rule(Rule::Array))]
     pub struct Array {
+        #[pest_ast(default(Vec::new()))]
         pub value: Vec<Value>,
     }
 
@@ -72,38 +110,17 @@ mod ast {
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Object))]
     pub struct Object {
-        pub pair: Vec<Pair>,
+        pub pair: Vec<Pair>
     }
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::ESon))]
     pub struct ESon {
-        pub Object: Object,
+        pub object: Object,
         _eoi: EOI,
     }
 
     #[derive(Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::EOI))]
     struct EOI;
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    use crate::parser::ast::ESon;
-    use from_pest::FromPest;
-    use pest::Parser;
-    use std::fs;
-
-    let source = String::from_utf8(fs::read("./src/examples/data.eson")?)?;
-    let mut parse_tree = ESonParser::parse(Rule::ESon, &source)?;
-    println!("parse tree = {parse_tree:#?}");
-    let syntax_tree: ESon = ESon::from_pest(&mut parse_tree).expect("infallible");
-    println!("syntax tree = {syntax_tree:#?}");
-    println!();
-
-    Ok(())
-}
-
-#[test]
-fn csv_example_runs() {
-    main().unwrap()
 }
