31ea84765c64ee28c660e64910bcb479a7520df2
jpig <isdolinskayaym@gmail.com>
2025-12-28 17:24:42 +0100

    add parser flat object unit test

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,7 +3,6 @@
 )]
 
 use pest_derive;
-use pest;
 
 #[derive(pest_derive::Parser)]
 #[grammar = "grammar/eson.pest"]
@@ -17,48 +16,49 @@ pub mod ast {
         span.as_str()
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Boolean))]
     pub struct Boolean {
         #[pest_ast(outer(with(span_into_str), with(str::parse), with(Result::unwrap)))]
-        value: bool
+        pub value: bool
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::String))]
     pub struct String<'pest> {
-        #[pest_ast(outer())]
-        value: pest::Span<'pest>
+        #[pest_ast(outer(with(span_into_str)))]
+        pub value: &'pest str
     }
+    
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Number))]
     pub struct Number {
         #[pest_ast(outer(with(span_into_str), with(str::parse), with(Result::unwrap)))]
-        value: f64
+        pub value: f64
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Infinity))]
     pub struct Infinity {}
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::undefined))]
     pub struct Undefined {}
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::NaN))]
     pub struct NaN {}
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::null))]
     pub struct Null {}
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::BigInt))]
     pub struct BigInt {}
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::value))]
     pub enum Value<'pest> {
         Object(Box<Object<'pest>>),
@@ -73,23 +73,23 @@ pub mod ast {
         BigInt(BigInt)
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::COMMENT))]
     pub struct Comment<'pest> {
-        #[pest_ast(outer())]
-        value: pest::Span<'pest>
+        #[pest_ast(outer(with(span_into_str)))]
+        pub value: &'pest str
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::key))]
     pub struct Key<'pest> {
-        #[pest_ast(outer())]
-        pub value: pest::Span<'pest>
+        #[pest_ast(outer(with(span_into_str)))]
+        pub value: &'pest str
     }
 
     //@TODO: научиться парсить комментарии, если перед ними идет запятая
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::pair))]
     pub struct Pair<'pest> {
         pub key: Key<'pest>,
@@ -97,28 +97,122 @@ pub mod ast {
         pub comment: Option<Comment<'pest>>
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Array))]
     pub struct Array<'pest> {
         #[pest_ast(default(Vec::new()))]
         pub value: Vec<Value<'pest>>,
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::Object))]
     pub struct Object<'pest> {
         pub pair: Vec<Pair<'pest>>
     }
 
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::ESon))]
     pub struct ESon<'pest> {
         pub object: Object<'pest>,
-        _eoi: EOI,
+        pub _eoi: EOI,
     }
 
-    #[derive(Debug, pest_ast::FromPest)]
+    #[derive(PartialEq, Debug, pest_ast::FromPest)]
     #[pest_ast(rule(Rule::EOI))]
-    struct EOI;
+    pub struct EOI;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use from_pest::FromPest;
+    use pest::Parser;
+    use crate::parser::ast;
+    use pretty_assertions::{assert_eq};
+
+    use super::*;
+
+    #[test]
+    fn test_flat_object() {
+        let source = String::from_utf8(std::fs::read("./examples/data.eson").unwrap()).unwrap();
+        let mut parse_tree = crate::parser::ESonParser::parse(crate::parser::Rule::ESon, &source).unwrap();
+        let syntax_tree: ast::ESon = ast::ESon::from_pest(&mut parse_tree).expect("infallible");
+
+        let expected = ast::ESon {
+            object: ast::Object {
+                pair: vec![
+                    ast::Pair {
+                        key: ast::Key { value: "name" },
+                        value: ast::Value::Number(ast::Number { value: 13.0 }),
+                        comment: None
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "value" },
+                        value: ast::Value::String(ast::String { value: "baz" }),
+                        comment: None
+                    },
+                    ast::Pair {
+                         key: ast::Key { value: "isDefined" },
+                         value: ast::Value::Boolean(ast::Boolean { value: false }),
+                         comment: None
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "isNotDefined" },
+                        value: ast::Value::Boolean(ast::Boolean { value: true }),
+                        comment: Some(ast::Comment { value: "// This is a response from some API"})
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "data" },
+                        value: ast::Value::Undefined(ast::Undefined {}),
+                        comment: None
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "valid_until" },
+                        value: ast::Value::NaN(ast::NaN {}),
+                        comment: None
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "valid_from" },
+                        value: ast::Value::Number(ast::Number { value: 1763225669356.0 }),
+                        comment: Some(ast::Comment { value : "/*\n    All cool guys use buffer like this:\n    [1, 2, 3] // Yes, it is a comment inside another one!\n  */"})
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "buffer"},
+                        value: ast::Value::Array(Box::new(ast::Array {
+                            value: vec![
+                                ast::Value::Number(ast::Number { value: 16.0 }),
+                                ast::Value::Number(ast::Number { value: 21.0 }),
+                                ast::Value::Number(ast::Number { value: 51.0 }),
+                                ast::Value::Number(ast::Number { value: 0.0 }),
+                                ast::Value::Number(ast::Number { value: 0.0 }),
+                                ast::Value::Number(ast::Number { value: 0.0 }),
+                                ast::Value::Number(ast::Number { value: 0.0 })
+                            ]
+                        })),
+                        comment: None
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "src"},
+                        value: ast::Value::Array(Box::new(ast::Array {
+                            value: vec![
+                                ast::Value::String(ast::String { value: "a" }),
+                                ast::Value::String(ast::String { value: "b" }),
+                                ast::Value::String(ast::String { value: "c" }),
+                            ]
+                        })),
+                        comment: None
+                    },
+                    ast::Pair {
+                        key: ast::Key { value: "source"},
+                        value: ast::Value::String(ast::String { value: "abc" }),
+                        comment: None
+                    }
+                ]
+            },
+            _eoi: ast::EOI
+        };
+
+        assert_eq!(&syntax_tree, &expected);
+    }
 }
